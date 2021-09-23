4c20ccdea21e82e893bb04b9c86d25990bc582b5
Yaser <62748315+YaserTheDev@users.noreply.github.com>
2021-09-23 17:56:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display');
+
+const buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button=>{
+    button.addEventListener('click',(e)=>{
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if(display.innerText)
+                    display.innerText = display.innerText.slice(0,-1);
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                }
+                catch{
+                    display.innerText = '';
+                    alert("Invalid operation");
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
